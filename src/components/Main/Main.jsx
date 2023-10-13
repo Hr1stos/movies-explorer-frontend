13dfec4dce5de926ce56e1835ed4a9f1b0ef3edf
Naumkin Ilya <naumkinilya@yandex.ru>
2023-10-13 23:28:06 +0300

    Сделать portfolio

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,10 @@
 import './Main.css';
 import Promo from './Promo/Promo'
 import NavTab from './NavTab/NavTab'
-import AboutProject from "./AboutProject/AboutProject"
-import Techs from "./Techs/Techs"
-import AboutMe from "./AboutMe/AboutMe"
-//import Portfolio from "./Portfolio/Portfolio"
+import AboutProject from './AboutProject/AboutProject'
+import Techs from './Techs/Techs'
+import AboutMe from './AboutMe/AboutMe'
+import Portfolio from './Portfolio/Portfolio'
 
 const Main = () => {
 	return (
@@ -14,7 +14,7 @@ const Main = () => {
 			<AboutProject />
 			<Techs />
 			<AboutMe />
-			{/*<Portfolio />*/}
+			<Portfolio />
 		</div>
 	)
 }
---
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -0,0 +1,48 @@
+import './Portfolio.css';
+
+const Portfolio = () => {
+	return (
+		<section className="portfolio">
+			<div className="portfolio__content">
+				<h4 className="portfolio__title">Портфолио</h4>
+				<ul className="portfolio__list">
+					<li className="portfolio__item">
+						<p className="portfolio__name">Статичный сайт</p>
+						<a
+							className="portfolio__link link"
+							href="https://hr1stos.github.io/How-to-learn/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							&#x2197;
+						</a>
+					</li>
+					<li className="portfolio__item">
+						<p className="portfolio__name">Адаптивный сайт</p>
+						<a
+							className="portfolio__link link"
+							href="https://hr1stos.github.io/HotelBooking/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							&#x2197;
+						</a>
+					</li>
+					<li className="portfolio__item">
+						<p className="portfolio__name">Одностраничное приложение</p>
+						<a
+							className="portfolio__link link"
+							href="https://github.com/Hr1stos/react-mesto-api-full-gha"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							&#x2197;
+						</a>
+					</li>
+				</ul>
+			</div>
+		</section>
+	)
+}
+
+export default Portfolio;
